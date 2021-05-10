97b72684e7b254faf2e9dbbb74a07de7e7509f63
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-05-10 18:54:11 -0500

    Finished lesson 13

---
+++ spotify_project/spotify/view.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from .credentials import REDIRECT_URI, CLIENT_SECRET, CLIENT_ID
+from rest_framework.views import APIView
+from requests import Request, post
+from rest_framework import status
+from rest_framework.response import Response
+from .util import update_or_create_user_tokens, is_spotify_authenticated
+
+
+class AuthURL(APIView):
+    def get(self, request, format=None):
+        # What we want to access from Spotify API
+        # Creates API endpoint
+        # Returns URL we can go to to verify our Spotify application
+        scopes = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
+
+        url = (
+            Request(
+                "GET",
+                "https://accounts.spotify.com/authorize",
+                parama={
+                    "scope": scopes,
+                    "response_type": "code",
+                    "redirect_uri": REDIRECT_URI,
+                    "client_id": CLIENT_ID,
+                },
+            )
+            .prepare()
+            .url
+        )
+
+        return Response({"url": url}, status=status.HTTP_200_OK)
+
+
+def spotify_callback(request, format=None):
+    # Get pieces of information from request that we want.
+    code = request.GET.get("code")
+    error = request.Get.get("error")
+
+    # Sends request back to spotify server asking for access token.
+    response = post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": REDIRECT_URI,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+    ).json()
+
+    access_token = response.get("access_token")
+    token_type = response.get("token_type")
+    refresh_token = response.get("refresh_token")
+    expires_in = response.get("expires_in")
+    error = response.get("error")
+
+    # Want to save tokens in database, located in util.py
+
+    # Make sure we have a session
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(
+        request.session.session_key, access_token, token_type, expires_in, refresh_token
+    )
+
+    return redirect("frontend:")
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(self.request.session.session_key)
+        return Response('status': is_authenticated}, status=status.HTTP_200_OK)
